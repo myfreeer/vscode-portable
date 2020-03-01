ab213e297488a40c10d441bea164de903ccd5556
myfreeer <myfreeer@users.noreply.github.com>
2020-03-01 20:33:10 +0800

    package: create packages for VSCodium (#5)
    Close #4

--- package.sh
+++ package.sh
@@ -5,11 +5,20 @@ cd "build_${MACHINE}"
 echo Packaging...
 url_64='https://vscode-update.azurewebsites.net/latest/win32-x64-archive/stable'
 url_32='https://vscode-update.azurewebsites.net/latest/win32-archive/stable'
+version_vscodium=$(curl -ks https://github.com/VSCodium/vscodium/releases/latest | grep -ioP '[0-9.]{4,}')
+name_vscodium_64="VSCodium-win32-x64-${version_vscodium}"
+name_vscodium_32="VSCodium-win32-ia32-${version_vscodium}"
+url_vscodium_64="https://github.com/VSCodium/vscodium/releases/download/${version_vscodium}/${name_vscodium_64}.zip"
+url_vscodium_32="https://github.com/VSCodium/vscodium/releases/download/${version_vscodium}/${name_vscodium_32}.zip"
 
 if echo "${MACHINE}" | grep -io x86_64; then
     url="${url_64}"
+    name_vscodium="${name_vscodium_64}"
+    url_vscodium="${url_vscodium_64}"
 else
     url="${url_32}"
+    name_vscodium="${name_vscodium_32}"
+    url_vscodium="${url_vscodium_32}"
 fi
 
 wget -nv --trust-server-names "${url}"
@@ -24,3 +33,19 @@ cd ..
 rm -rf "${zip_name}"
 cd ..
 which appveyor.exe && appveyor.exe PushArtifact "${zip_name}.7z"
+
+rm -rf "${zip_name}.7z"
+
+# begin vscodium
+cd "build_${MACHINE}"
+wget -nv "${url_vscodium}"
+mkdir -p "${name_vscodium}"
+cd "${name_vscodium}"
+/usr/bin/unzip -q "../${name_vscodium}.zip" && rm -f "../${name_vscodium}.zip"
+cp ../version.dll  ./version.dll
+7z a -mx9 -slp "../../${name_vscodium}.7z" .
+cd ..
+rm -rf "${name_vscodium}"
+cd ..
+which appveyor.exe && appveyor.exe PushArtifact "${name_vscodium}.7z"
+# end vscodium
